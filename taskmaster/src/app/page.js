3643af0dc63b8b4145f0641c3a074e70346bf777
Shahzad <muhammedshahzad012@gmail.com>
2023-11-07 03:14:17 +0530

    create not-found.js

--- taskmaster/src/app/page.js
+++ taskmaster/src/app/page.js
@@ -1,22 +1,20 @@
-import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main><div class="bg-gray-800 text-white p-4 flex items-center justify-between">
 
     <div class="text-3xl font-semibold">Taskmaster</div>
-
-
-    <button class="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded">
+    <Link class="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded transition duration-300 ease-in-out " href="/login">
       Login
-    </button>
+    </Link>
   </div>
+  
   <div class="flex justify-center items-center h-screen">
-  <div class="flex justify-center items-center h-screen">
-  <button class="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-6 px-14 rounded-full">
-  Unlock Your Productivity Potential!
-  </button>
-</div>
+  <Link class='bg-blue-500 hover:bg-blue-700 text-white font-semibold py-6 px-14 rounded-full transition duration-300 ease-in-out' href="/signup">
+
+Unlock Your Productivity Potential!
+  </Link>
 </div>
 
   </main>
